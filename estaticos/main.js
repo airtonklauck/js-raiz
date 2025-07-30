a75707319f86d11000618cf5d482bd51b1b9c18a
Airton Vicente Klauck <airtonklauck@gmail.com>
2025-07-30 20:41:20 -0300

    Começando abstração da DOM API para tornar declarativa a escrita da interface

--- estaticos/main.js
+++ estaticos/main.js
@@ -1,23 +1,54 @@
 import dados from './dados.js'
 
-function templateCardapio(menu) {
-    return `
-        <div class="cardapio" style="color: red">
-            <header>
-                <h3>${menu.title} - ${menu.restaurante.nome}</h3>
-            </header>
-            <div class="cardapio-body">
-                <ul>
-                ${menu.sections.map(section =>
-        `<li>${section.title}</li>`
-    ).join('')}
-                </ul>
-            </div>  
-        </div>
-    `
+function elemento(tagNome, atributos, filhos) {
+
+    /**
+     * Se o segundo argumento (atributos) for um array ou string, significa que não
+     * temos atributos
+     * Os filhos podem ser array ou string
+     * Se o segundo argumento for objeto, temos atributos
+     */
+
+    const $elemento = document.createElement(tagNome)
+    const _filhos = (Array.isArray(atributos) || typeof atributos === 'string')
+        ? atributos : filhos
+
+    if (typeof _filhos === 'string') {
+        $elemento.appendChild(
+            document.createTextNode(_filhos)
+        )
+    } else {
+        for (let $filho of _filhos) {
+            $elemento.appendChild($filho)
+        }
+    }
+
+    return $elemento
+
 }
 
-const templateCardapios = Array.from(dados.menus.values())
+function templateCardapio(menu) {
+    return elemento('div', [
+        elemento('header', [
+            elemento('h3',
+                { style: 'color: red' },
+                `${menu.title} - ${menu.restaurante.nome}`
+            )
+        ]),
+        elemento('div', [
+            elemento('ul',
+                menu.sections.map(section =>
+                    elemento('li', section.title)
+                )
+            )
+        ])
+    ])
+}
+
+const $cardapios = document.querySelector('.cardapios')
+const $fragment = document.createDocumentFragment()
+
+Array.from(dados.menus.values())
     .slice(3)
     .map(menu => ({
         ...menu,
@@ -25,10 +56,12 @@ const templateCardapios = Array.from(dados.menus.values())
             nome: dados.restaurantes.get(menu.restauranteId).name
         }
     }))
-    .map(templateCardapio)
-    .join('')
+    .forEach(menu =>
+        $fragment.appendChild(templateCardapio(menu))
+    )
 
-const $cardapios = document.querySelector('.cardapios')
-$cardapios.insertAdjacentHTML('beforeend', templateCardapios)
+$cardapios.appendChild($fragment)
+
+// $cardapios.insertAdjacentHTML('beforeend', templateCardapios)
 
 // console.log('Renderizando', templateCardapios)
