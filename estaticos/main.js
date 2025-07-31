5d7b79aa4f8ce35d65625d1ea6103c795aade8f4
Airton Vicente Klauck <airtonklauck@gmail.com>
2025-07-31 15:36:47 -0300

    Abstração da DOM API para tornar declarativa a escrita da interface - Aceitando e colocando classes e id nos elementos - Após aulão de RegExp

--- estaticos/main.js
+++ estaticos/main.js
@@ -29,6 +29,31 @@ function _transformarEmArray(valor) {
     return Array.isArray(valor) ? valor : [valor]
 }
 
+function _extrairNomeDaTag(tagNome) {
+    return tagNome.match(/^(\w+)/)[0]
+}
+
+function _extrairClassesEId(tagNome) {
+
+    return Array.from(tagNome.matchAll(/[#.]{1}([\w\-]*)/g))
+        .reduce(
+            (acumulador, atual) => {
+
+                const [match, grupoDeCaptura] = atual
+
+                if (match.startsWith('.'))
+                    acumulador.classes.push(grupoDeCaptura)
+
+                if (match.startsWith('#'))
+                    acumulador.id = grupoDeCaptura
+
+                return acumulador
+            }
+            ,
+            { classes: [], id: '' }
+        )
+}
+
 function elemento(tagNome, atributos, filhos) {
 
     /**
@@ -38,7 +63,14 @@ function elemento(tagNome, atributos, filhos) {
      * Se o segundo argumento for objeto, temos atributos
      */
 
-    const $elemento = document.createElement(tagNome)
+    const $elemento = document.createElement(_extrairNomeDaTag(tagNome))
+
+    const { classes, id } = _extrairClassesEId(tagNome)
+
+    if (id) $elemento.id = id
+
+    if (classes.length)
+        $elemento.classList.add(...classes)
 
     const _filhos = _ehFilho(atributos)
         ? atributos : filhos
@@ -58,16 +90,11 @@ function elemento(tagNome, atributos, filhos) {
 }
 
 function templateCardapio(menu) {
-    return elemento('div', [
-        elemento('header', [
-            elemento('h3', { style: 'color: red' }, `${menu.title} - ${menu.restaurante.nome}`),
-            elemento('div', [
-                elemento('span', 'Olá'),
-                'mundo',
-                elemento('strong', '!!!')
-            ])
+    return elemento('div#card.card.cardapio', [
+        elemento('header.card-header', [
+            elemento('h3.card-title', { style: 'color: red' }, `${menu.title} - ${menu.restaurante.nome}`),
         ]),
-        elemento('div', [
+        elemento('div.card-body#cardBody', [
             elemento('ul',
                 menu.sections.map(section =>
                     elemento('li', section.title)
